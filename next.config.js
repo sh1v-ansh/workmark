55ccf715e4f845f74e90d4c7190a434e66def2fe
Shivansh Soni <80147943+sh1v-ansh@users.noreply.github.com>
2026-04-14 14:33:45 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [],
   },
 }
 
-export default nextConfig
+module.exports = nextConfig
